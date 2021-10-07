9ee28ee263a1e205d2cb4b12c0760929f2c7d705
roman <rcecile@reseau.free.fr>
2021-10-07 23:00:14 +0200

    Drag and drop group to change order

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 // import PropTypes from "prop-types";
 
+// DnD
+import Sortable from "sortablejs";
+
 // Styles
 import "../../styles/Board/index.css";
 
@@ -11,26 +14,53 @@ import Group from "../Group";
 import { GroupContext } from "../context/group-context";
 
 const Board = () => {
-  const [groups, setGroups] = useState([]);
-  const groupModel = {
-    id: Date.now(),
-    title: "Titre",
-  };
+  const [groups, setGroups] = useState([
+    {
+      id: Date.now(),
+      title: "Titre",
+    },
+  ]);
+  const simpleList = document.getElementById("simpleList");
+  if (simpleList) {
+    Sortable.create(simpleList, {
+      filter: ".add-button",
+      animation: 150,
+      draggable: ".group",
+    });
+  }
+
+  // Scroll with mouse wheel
+  const item = document.getElementById("simpleList");
+  window.addEventListener("wheel", function (e) {
+    if (item) {
+      if (e.deltaY > 0) item.scrollLeft += 3;
+      else item.scrollLeft -= 3;
+    }
+  });
 
   const handleClick = () => {
-    setGroups((prevState) => [...prevState, groupModel]);
+    setGroups((prevState) => [
+      {
+        id: Date.now(),
+        title: "Titre",
+      },
+      ...prevState,
+    ]);
   };
+
   return (
     <div className="board">
       <GroupContext.Provider value={groups}>
-        {groups.map((group) => (
-          <Group key={group.id} group={group} setGroups={setGroups} />
-        ))}
+        <div id="simpleList" className="drag-list">
+          <button id="addButton" onClick={handleClick} className="add-button">
+            {" "}
+            Ajouter
+          </button>
+          {groups.map((group) => (
+            <Group key={group.id} group={group} setGroups={setGroups} />
+          ))}
+        </div>
       </GroupContext.Provider>
-      <button onClick={handleClick} className="add-button">
-        {" "}
-        + Ajouter
-      </button>
     </div>
   );
 };
